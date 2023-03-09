d08881876967921f961a5a0f01370211a68bbfd9
ceddy2001 <cedricklingler@gmail.com>
2023-03-09 15:04:27 +0100

    first try of lower branch

---
+++ pap_to_code.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Hast du Alkohol GEtrunken? (y/n)")
+alkohol = input()
+
+if alkohol == y:
+
+        i + 1
+
+
+
+elif alkohol == n: 
+    print("Nach Hause fahren")
